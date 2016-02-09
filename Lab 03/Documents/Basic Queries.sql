10cbace1ce35337c76beaf7cf1b25fd10c003e3b
Tienza <Piradon.Liengtiraphan1@gmail.com>
2016-02-09 11:53:18 -0500

    Edited script to be more aesthetically pleasing

--- Lab 03/Documents/Basic Queries.sql
+++ Lab 03/Documents/Basic Queries.sql
@@ -5,36 +5,36 @@ FROM orders;
 -- #2 List the name and city of agents named Smith.
 SELECT name, city
 FROM agents
-WHERE name='Smith';
+WHERE name = 'Smith';
 
 -- #3 List the pid, name, and priceUSD of products with quantity more than 208,000.
 SELECT pid, name, priceUSD
 FROM products
-WHERE quantity>208000;
+WHERE quantity > 208000;
 
 -- #4 List the names and cities of customers in Dallas.
 SELECT name, city
 FROM customers
-WHERE city='Dallas';
+WHERE city = 'Dallas';
 
 -- #5 List the names of agents not in New York and not in Tokyo.
 SELECT name
 FROM agents
 WHERE city <> 'New York'
-AND city <> 'Tokyo';
+  AND city <> 'Tokyo';
 
 -- #6 List all data for products not in Dallas or Duluth that cost US$1 or more.
 SELECT *
 FROM products
 WHERE city <> 'Dallas'
-AND city <> 'Duluth'
-AND priceUSD >= 1;
+  AND city <> 'Duluth'
+  AND priceUSD >= 1;
 
 -- #7 List all data for orders in January or March.
 SELECT *
 FROM orders
 WHERE mon = 'jan'
-OR mon = 'mar';
+   OR mon = 'mar';
 
 -- #8 List all orders from the customer whose cid is C005.
 SELECT *
