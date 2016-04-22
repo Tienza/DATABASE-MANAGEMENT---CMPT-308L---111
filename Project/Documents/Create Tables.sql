53cbd524c5b22fe59fcc842e82df2b06594d660d
Tienza <Piradon.Liengtiraphan1@gmail.com>
2016-04-22 17:36:10 -0400

    Finsihed creating tables down to Armaments, will continue after help with event

--- Project/Documents/Create Tables.sql
+++ Project/Documents/Create Tables.sql
@@ -111,6 +111,26 @@ INSERT INTO geneseedBank(pchid, progenitorFname, progenitorLname)
 	VALUES(1, 'Hadd', 'Buisson');
 INSERT INTO geneseedBank(pchid, progenitorFname, progenitorLname)
 	VALUES(1, 'Thormothr', 'Beigarth');
+INSERT INTO geneseedBank(pchid, progenitorFname, progenitorLname)
+	VALUES(1, 'Gislrr', 'Svefgar');
+INSERT INTO geneseedBank(pchid, progenitorFname, progenitorLname)
+	VALUES(1, 'Ortinal', 'Vidund');
+INSERT INTO geneseedBank(pchid, progenitorFname, progenitorLname)
+	VALUES(1, 'Vonmir', 'Gorurd');
+INSERT INTO geneseedBank(pchid, progenitorFname, progenitorLname)
+	VALUES(1, 'Hamlsvar', 'Colossus');
+INSERT INTO geneseedBank(pchid, progenitorFname, progenitorLname)
+	VALUES(1, 'Kunaug', 'Argarkesson');
+INSERT INTO geneseedBank(pchid, progenitorFname, progenitorLname)
+	VALUES(1, 'Lorer', 'Tarbeldson');
+INSERT INTO geneseedBank(pchid, progenitorFname, progenitorLname)
+	VALUES(1, 'Yngvic', 'Asahriksen');
+INSERT INTO geneseedBank(pchid, progenitorFname, progenitorLname)
+	VALUES(1, 'Algoleif', 'Engmerssen');
+INSERT INTO geneseedBank(pchid, progenitorFname, progenitorLname)
+	VALUES(1, 'Ormhelm', 'Argardessen');
+INSERT INTO geneseedBank(pchid, progenitorFname, progenitorLname)
+	VALUES(1, 'Ingmrbald', 'Greycloak');
 
 SELECT * FROM geneseedBank;
 
@@ -269,14 +289,158 @@ INSERT INTO astartes(rid, sid, gcid, fName, lName, serviceStart)
 	VALUES(2, 4, 12, 'Kjarl', 'Grimblood', '602 962.M39');
 INSERT INTO astartes(rid, sid, gcid, fName, lName, serviceStart)
 	VALUES(2, 5, 13, 'Bulveye', 'Axeman of Russ', '313 169.M30');
+INSERT INTO astartes(rid, sid, gcid, fName, lName, serviceStart)
+	VALUES(7, 2, 1, 'Bjorn', 'Fell-Handed', '359 417.M31');
+INSERT INTO astartes(rid, sid, gcid, fName, lName, serviceStart)
+	VALUES(7, 2, 1, 'Kverlaf', 'Murderfang', '273 678.M34');
 
 SELECT * FROM astartes;
 
 --------------------------------------------------------------------
 
+-- Create Gene-Seed History Table --
+DROP TABLE IF EXISTS geneseedHistory;
 
+CREATE TABLE geneseedHistory (
+	gsid		INT NOT NULL REFERENCES geneseedBank(gsid),
+	aid		INT NOT NULL REFERENCES astartes(aid),
+	dateOfImplant	TEXT NOT NULL,
+	primary key(gsid, aid)
+);
 
+-- Insert into Gene-Seed History --
+INSERT INTO geneseedHistory(gsid, aid, dateOfImplant)
+	VALUES(30, 1, '643 830.M33');
+INSERT INTO geneseedHistory(gsid, aid, dateOfImplant)
+	VALUES(1, 2, '318 077.M32');
+INSERT INTO geneseedHistory(gsid, aid, dateOfImplant)
+	VALUES(2, 3, '911 219.M39');
+INSERT INTO geneseedHistory(gsid, aid, dateOfImplant)
+	VALUES(3, 4, '541 971.M34');
+INSERT INTO geneseedHistory(gsid, aid, dateOfImplant)
+	VALUES(4, 5, '012 578.M37');
+INSERT INTO geneseedHistory(gsid, aid, dateOfImplant)
+	VALUES(5, 6, '569 896.M38');
+INSERT INTO geneseedHistory(gsid, aid, dateOfImplant)
+	VALUES(6, 7, '616 102.M31');
+INSERT INTO geneseedHistory(gsid, aid, dateOfImplant)
+	VALUES(7, 8, '161 999.M36');
+INSERT INTO geneseedHistory(gsid, aid, dateOfImplant)
+	VALUES(8, 9, '210 645.M34');
+INSERT INTO geneseedHistory(gsid, aid, dateOfImplant)
+	VALUES(9, 10, '474 651.M36');
+INSERT INTO geneseedHistory(gsid, aid, dateOfImplant)
+	VALUES(10, 11, '480 604.M34');
+INSERT INTO geneseedHistory(gsid, aid, dateOfImplant)
+	VALUES(11, 12, '452 065.M38');
+INSERT INTO geneseedHistory(gsid, aid, dateOfImplant)
+	VALUES(12, 13, '672 532.M29');
+INSERT INTO geneseedHistory(gsid, aid, dateOfImplant)
+	VALUES(13, 14, '519 441.M30');
+INSERT INTO geneseedHistory(gsid, aid, dateOfImplant)
+	VALUES(14, 15, '966 203.M33');
+
+SELECT * FROM geneseedHistory
+
+--------------------------------------------------------------------
+
+-- Create Dreadnought Table --
+DROP TABLE IF EXISTS dreadnought;
+
+CREATE TABLE dreadnought (
+	did		SERIAL UNIQUE NOT NULL,
+	dreadType	TEXT NOT NULL,
+	primary key(did)
+);
+
+-- Insert into Dreadnought --
+INSERT INTO dreadnought(dreadType)
+	VALUES('Venerable');
+INSERT INTO dreadnought(dreadType)
+	VALUES('Siege');
+INSERT INTO dreadnought(dreadType)
+	VALUES('Ironclad');
+INSERT INTO dreadnought(dreadType)
+	VALUES('Hellfire');
+INSERT INTO dreadnought(dreadType)
+	VALUES('Contemptor Pattern');
+INSERT INTO dreadnought(dreadType)
+	VALUES('Deredeo');
+INSERT INTO dreadnought(dreadType)
+	VALUES('Librarian');
+INSERT INTO dreadnought(dreadType)
+	VALUES('Chaplain');
+INSERT INTO dreadnought(dreadType)
+	VALUES('Leviathan');
+
+SELECT * FROM dreadnought;
+
+--------------------------------------------------------------------
+
+-- Create Active Dreadnought Table --
+DROP TABLE IF EXISTS activeDreadnought;
+
+CREATE TABLE activeDreadnought (
+	did		INT UNIQUE NOT NULL REFERENCES dreadnought(did),
+	aid		INT UNIQUE NOT NULL REFERENCES astartes(aid),
+	primary key(did, aid)
+);
+
+-- Insert into Active Dreadnought --
+INSERT INTO activeDreadnought(did, aid)
+	VALUES(1, 14);
+INSERT INTO activeDreadnought(did, aid)
+	VALUES(3, 15);
+
+SELECT * FROM activeDreadnought;
+
+--------------------------------------------------------------------
+
+-- Create Armaments Table --
+DROP TABLE IF EXISTS armaments;
+
+CREATE TABLE armaments (
+	eid		SERIAL UNIQUE NOT NULL,
+	type		TEXT NOT NULL,
+	mrkDesignation	Char(12) NOT NULL,
+	eName		TEXT NOT NULL,
+	primary key(eid)
+);
+
+-- Insert into Armaments --
+INSERT INTO armaments(type, mrkDesignation, eName)
+	VALUES('Weapon', 'VB', 'Godwyn Pattern Bolter');
+INSERT INTO armaments(type, mrkDesignation, eName)
+	VALUES('Weapon', 'IV', 'Ultra Pattern Bolter');
+INSERT INTO armaments(type, mrkDesignation, eName)
+	VALUES('Weapon', 'I', 'Lighting Claws');
+INSERT INTO armaments(type, mrkDesignation, eName)
+	VALUES('Weapon', 'I', 'Force Sword');
+INSERT INTO armaments(type, mrkDesignation, eName)
+	VALUES('Weapon', 'I', 'Doom Hammer');
+INSERT INTO armaments(type, mrkDesignation, eName)
+	VALUES('Weapon', 'Xf', 'Krakentooth Pattern Chainsword');
+INSERT INTO armaments(type, mrkDesignation, eName)
+	VALUES('Armour', 'I', 'Thunder Pattern Power Armour');
+INSERT INTO armaments(type, mrkDesignation, eName)
+	VALUES('Armour', 'II', 'Crusade Pattern Power Armour');
+INSERT INTO armaments(type, mrkDesignation, eName)
+	VALUES('Armour', 'III', 'Iron Pattern Power Armour');
+INSERT INTO armaments(type, mrkDesignation, eName)
+	VALUES('Armour', 'IV', 'Maximus Pattern Power Armour');
+INSERT INTO armaments(type, mrkDesignation, eName)
+	VALUES('Armour', 'V', 'Heresy Pattern Power Armour');
+INSERT INTO armaments(type, mrkDesignation, eName)
+	VALUES('Armour', 'VI', 'Corcus Pattern Power Armour');
+INSERT INTO armaments(type, mrkDesignation, eName)
+	VALUES('Armour', 'VII', 'Aquila Pattern Power Armour');
+INSERT INTO armaments(type, mrkDesignation, eName)
+	VALUES('Armour', 'VIII', 'Errant Pattern Power Armour');
+
+SELECT * FROM armaments;
 --------------------------------------------------------------------
 
 -- Test Quries --
 -- SELECT * FROM greatCompany g INNER JOIN astartes a ON a.gcid = g.gcid INNER JOIN rank r ON r.rid = a.rid INNER JOIN specialization s ON a.sid = s.sid INNER JOIN vlkaFenryka v ON g.pchid = v.chid;
+-- SELECT * FROM geneseedHistory gh INNER JOIN astartes a ON gh.aid = a.aid INNER JOIN geneseedBank gs ON gs.gsid = gh.gsid;
+-- SELECT * FROM activeDreadnought ad INNER JOIN dreadnought d ON ad.did = d.did INNER JOIN astartes a ON ad.aid = a.aid INNER JOIN geneseedHistory gsh ON gsh.aid = a.aid INNER JOIN geneseedBank gsb on gsb.gsid = gsh.gsid;
